d3f4d074f23ff4cc83eae5f40df402a599439b7d
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-03-16 07:50:02 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,27 +1,39 @@
-class food{
-    constructor
-        var foodStock
-        var lastFed
-
-        };
-
+class Food{
+    constructor(){
+        this.image = loadImage("images/Milk.png");
     }
+
+    getFoodStock(){
+        var foodStockRef = database.ref('foodStock');
+        foodStockRef.on("value",function(data){
+            foodS = data.val();
+        });
+    }
+
+    updateFoodStock(s){
+        database.ref('/').update({
+            foodStock: s
+        });
+    }
+
     display(){
-        var x=80,y=100;
 
+        var x = 80, y = 100;
         imageMode(CENTER);
-        image(this.image,720,220,70,70);
+        image(this.image,700,300,70,70);
 
-        if(this.foodStock!=0){
-            for(var i=0;i<this.foodStock;++){
-                if(i%10==0){
-                    x=80
-                    y=y+50;
+        if (foodS !== 0){
+            for (var i=0; i<foodS; i++){
+
+                if (i%10 === 0){
+                    y = y+50;
+                    x = 80;
                 }
                 image(this.image,x,y,50,50);
-                x=x+30;
+                x = x+30;
             }
         }
-    }
 
-        
+
+    }
+}
